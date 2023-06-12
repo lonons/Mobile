3246b08747a72f00b21abe7e3bc29ca1d28f4b08
lonons <vkolay01@mail.ru>
2023-06-12 22:57:46 +0700

    firstCommitLesson1

---
+++ Assets/_Root/Scripts/Ui/MainMenuView.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+namespace Ui
+{
+    public class MainMenuView : MonoBehaviour
+    {
+        [SerializeField] private Button _buttonStart;
+        [SerializeField] private Button _buttonSettings;
+
+
+        public void Init(UnityAction startGame, UnityAction openSettings)
+        {
+            _buttonStart.onClick.AddListener(startGame);
+            _buttonSettings.onClick.AddListener(openSettings);
+        }
+           
+        
+
+        public void OnDestroy() =>
+            _buttonStart.onClick.RemoveAllListeners();
+    }
+}
